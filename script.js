b299fd63809c21595269520fd051d35ec8c51853
nox1d <114980767+nox1d@users.noreply.github.com>
2023-07-31 15:51:44 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -19,3 +19,40 @@ function getComputerChoice(){
     return computerChoice;
 }
 
+// check playerSelection
+// check computerSelection
+// scissors beat paper, paper beats rock, rock beats scissors
+
+function playRound(playerSelection, computerSelection){
+    let roundResult;
+    let playerScore = 0;
+    let computerScore = 0;
+    
+    if (!(playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors")) {
+        roundResult = `${playerSelection} is invalid`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        roundResult = `You Win! Rock beats Scissors`;   
+        playerScore += 1; 
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+        roundResult = `You Win! Paper beats Rock`;
+        playerScore += 1; 
+    } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        roundResult = `You Win! Scissors beat Paper`;
+        playerScore += 1; 
+    } else if (playerSelection === "scissors" && computerSelection === "rock"){
+        roundResult = `You Lose! Rock beats Scissors`;
+        computerScore += 1; 
+    } else if (playerSelection === "rock" && computerSelection === "paper"){
+        roundResult = `You Lose! Paper beats Rock`;
+        computerScore += 1;
+    } else if (playerSelection === "paper" && computerSelection === "scissors"){
+        roundResult = `You Lose! Scissors beat Paper`;
+        computerScore += 1;
+    } else {
+        roundResult = `Both of you chose ${playerSelection}, It's a draw!`;
+    };
+    
+    
+    return roundResult;
+}
+
